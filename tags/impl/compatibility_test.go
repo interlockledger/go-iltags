8d1997905a88761a64e5210df2a3f1eb940e7852
Fabio Jun Takada Chino <fchino@il2.io>
2022-05-09 18:04:53 -0300

    Compatibility test of implicit tags completed.

---
+++ tags/impl/compatibility_test.go
@@ -0,0 +1,256 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, InterlockLedger Network
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package impl
+
+import (
+	"bytes"
+	"testing"
+
+	. "github.com/interlockledger/go-iltags/tags"
+	"github.com/stretchr/testify/assert"
+)
+
+// This file contains a few serialization/deserialization tests used to ensure
+// compatibility with the defined standard
+
+var testTagFactory = NewStandardTagFactory(false)
+
+func TestNullTagReference(t *testing.T) {
+
+	serialized := []byte{0x00}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_NULL_TAG_ID, tag.Id())
+
+	assert.IsType(t, &NullTag{}, tag)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestBoolTagReference(t *testing.T) {
+
+	serialized := []byte{0x01, 0x01}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_BOOL_TAG_ID, tag.Id())
+	assert.IsType(t, &BoolTag{}, tag)
+	assert.True(t, tag.(*BoolTag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestInt8TagReference(t *testing.T) {
+
+	serialized := []byte{0x02, 0xFE}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_INT8_TAG_ID, tag.Id())
+	assert.IsType(t, &Int8Tag{}, tag)
+	assert.Equal(t, int8(-2), tag.(*Int8Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestUInt8TagReference(t *testing.T) {
+
+	serialized := []byte{0x03, 0xFE}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_UINT8_TAG_ID, tag.Id())
+	assert.IsType(t, &UInt8Tag{}, tag)
+	assert.Equal(t, uint8(0xFE), tag.(*UInt8Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestInt16TagReference(t *testing.T) {
+
+	serialized := []byte{0x04, 0xFE, 0xDC}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_INT16_TAG_ID, tag.Id())
+	assert.IsType(t, &Int16Tag{}, tag)
+	assert.Equal(t, int16(-292), tag.(*Int16Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestUInt16TagReference(t *testing.T) {
+
+	serialized := []byte{0x05, 0xFE, 0xDC}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_UINT16_TAG_ID, tag.Id())
+	assert.IsType(t, &UInt16Tag{}, tag)
+	assert.Equal(t, uint16(0xFEDC), tag.(*UInt16Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestInt32TagReference(t *testing.T) {
+
+	serialized := []byte{0x06, 0xFE, 0xDC, 0xBA, 0x98}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_INT32_TAG_ID, tag.Id())
+	assert.IsType(t, &Int32Tag{}, tag)
+	assert.Equal(t, int32(-19088744), tag.(*Int32Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestUInt32TagReference(t *testing.T) {
+
+	serialized := []byte{0x07, 0xFE, 0xDC, 0xBA, 0x98}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_UINT32_TAG_ID, tag.Id())
+	assert.IsType(t, &UInt32Tag{}, tag)
+	assert.Equal(t, uint32(0xFACA0123), tag.(*UInt32Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestInt64TagReference(t *testing.T) {
+
+	serialized := []byte{0x08, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_INT64_TAG_ID, tag.Id())
+	assert.IsType(t, &Int64Tag{}, tag)
+	assert.Equal(t, int64(-81985529216486896), tag.(*Int64Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestUInt64TagReference(t *testing.T) {
+
+	serialized := []byte{0x09, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_UINT64_TAG_ID, tag.Id())
+	assert.IsType(t, &UInt64Tag{}, tag)
+	assert.Equal(t, uint64(0xFEDCBA9876543210), tag.(*UInt64Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestILIntTagReference(t *testing.T) {
+
+	serialized := []byte{0x0A, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_ILINT_TAG_ID, tag.Id())
+	assert.IsType(t, &ILIntTag{}, tag)
+	assert.Equal(t, uint64(0xdcba9876543308), tag.(*ILIntTag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestFloat32TagReference(t *testing.T) {
+
+	serialized := []byte{0x0B, 0xFE, 0xDC, 0xBA, 0x98}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_BIN32_TAG_ID, tag.Id())
+	assert.IsType(t, &Float32Tag{}, tag)
+	assert.Equal(t, float32(-1.466995e+38), tag.(*Float32Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestFloat64TagReference(t *testing.T) {
+
+	serialized := []byte{0x0C, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_BIN64_TAG_ID, tag.Id())
+	assert.IsType(t, &Float64Tag{}, tag)
+	assert.Equal(t, float64(-1.2313300687736946e+303), tag.(*Float64Tag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestFloat128TagReference(t *testing.T) {
+
+	serialized := []byte{0x0D, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x10, 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_BIN128_TAG_ID, tag.Id())
+	assert.IsType(t, &Float128Tag{}, tag)
+	assert.Equal(t, serialized[1:], tag.(*Float128Tag).Payload[:])
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
+
+func TestSignedILIntTagReference(t *testing.T) {
+
+	serialized := []byte{0x0E, 0xFE, 0xDC, 0xBA, 0x98, 0x76, 0x54, 0x32, 0x11}
+	r := bytes.NewReader(serialized)
+	tag, err := ILTagDeserialize(testTagFactory, r)
+	assert.Nil(t, err)
+	assert.Equal(t, IL_SIGNED_ILINT_TAG_ID, tag.Id())
+	assert.IsType(t, &SignedILIntTag{}, tag)
+	assert.Equal(t, int64(-31064829429684613), tag.(*SignedILIntTag).Payload)
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, ILTagSeralize(tag, w))
+	assert.Equal(t, serialized, w.Bytes())
+}
