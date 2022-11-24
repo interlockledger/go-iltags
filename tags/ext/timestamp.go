295c24f0bbed9513b95bf9ecbff479099a441d3f
Fabio Jun Takada Chino <fchino@il2.io>
2022-11-24 20:26:17 -0300

    Generic timestamp tags implemented (#1).

---
+++ tags/ext/timestamp.go
@@ -0,0 +1,206 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, InterlockLedger Network
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package ext
+
+import (
+	"io"
+	"time"
+
+	"github.com/interlockledger/go-iltags/serialization"
+	"github.com/interlockledger/go-iltags/tags"
+	"github.com/interlockledger/go-iltags/tags/impl"
+)
+
+/*
+TimestapTag is a generic timestamp tag that stores the timestamp as a signed
+ILInt value in microseconds since 1970-01-01T00:00:00.000000Z.
+
+Since it is not a standard tag it does not have a Standard tag ID associated
+with it.
+*/
+type TimestapTag struct {
+	impl.SignedILIntTag
+}
+
+/*
+Create a new TimestapTag. This tag cannot
+*/
+func NewTimestapTag(id tags.TagID) *TimestapTag {
+	if id.Implicit() {
+		panic("This tag cannot have an implicit tag id.")
+	}
+	var t TimestapTag
+	t.SetId(id)
+	return &t
+}
+
+/*
+Sets the timestamp value. It will update TimestapTag.Payload with the
+appropriate value.
+*/
+func (t *TimestapTag) SetTimestamp(ts time.Time) {
+	t.Payload = ts.UnixMicro()
+}
+
+/*
+Recovers the stored value as a time.Time instance. The returned value will be
+at the local time zone.
+*/
+func (t *TimestapTag) GetTimestamp() time.Time {
+	return time.UnixMicro(t.Payload)
+}
+
+/*
+Recovers the stored value as a time.Time instance. The returned value will be
+at UTC.
+*/
+func (t *TimestapTag) GetTimestampUTC() time.Time {
+	return t.GetTimestamp().UTC()
+}
+
+//------------------------------------------------------------------------------
+
+// Payload of a timestamp with timezone information. This payload does store the
+// offset but not the original name of the timezone.
+type TimestampTZPayload struct {
+	impl.SignedILIntPayload
+	/*
+		Timezone offset in minutes from UTC. For example a UTC-03:00 will be
+		represented as -180 while UTC+04:00 will be represented as 240.
+	*/
+	Offset int16
+}
+
+// Implementation of ILTagPayload.ValueSize().
+func (p *TimestampTZPayload) ValueSize() uint64 {
+	return p.SignedILIntPayload.ValueSize() + 2
+}
+
+// Implementation of ILTagPayload.SerializeValue()
+func (p *TimestampTZPayload) SerializeValue(writer io.Writer) error {
+	if err := p.SignedILIntPayload.SerializeValue(writer); err != nil {
+		return nil
+	}
+	return serialization.WriteInt16(writer, p.Offset)
+}
+
+/*
+Implementation of ILTagPayload.DeserializeValue().
+*/
+func (p *TimestampTZPayload) DeserializeValue(factory tags.ILTagFactory, valueSize int, reader io.Reader) error {
+	if valueSize < 3 {
+		return tags.ErrBadTagFormat
+	}
+	r := &io.LimitedReader{R: reader, N: int64(valueSize)}
+	if err := p.SignedILIntPayload.DeserializeValue(factory, valueSize, r); err != nil {
+		return tags.ErrBadTagFormat
+	}
+	if r.N != 2 {
+		return tags.ErrBadTagFormat
+	}
+	if offset, err := serialization.ReadInt16(reader); err != nil {
+		return tags.ErrBadTagFormat
+	} else {
+		p.Offset = offset
+	}
+	return nil
+}
+
+//------------------------------------------------------------------------------
+
+/*
+TimestapTag is a generic timestamp tag that stores the timestamp as a signed
+ILInt value in microseconds since 1970-01-01T00:00:00.000000Z and the timezone
+original timezone information (except for the name of the timezone).
+
+Since it is not a standard tag it does not have a Standard tag ID associated
+with it.
+*/
+type TimestapTZTag struct {
+	impl.ILTagHeaderImpl
+	TimestampTZPayload
+}
+
+/*
+Create a new TimestapTag. This tag cannot
+*/
+func NewTimestapTZTag(id tags.TagID) *TimestapTZTag {
+	if id.Implicit() {
+		panic("This tag cannot have an implicit tag id.")
+	}
+	var t TimestapTZTag
+	t.SetId(id)
+	return &t
+}
+
+/*
+Sets the timestamp value. It will update TimestapTag.Payload with the
+appropriate value.
+*/
+func (t *TimestapTZTag) SetTimestamp(ts time.Time) {
+	t.Payload = ts.UnixMicro()
+	_, offs := ts.Zone()
+	t.Offset = int16(offs / 60)
+}
+
+/*
+Returns the timezone. Since the name is not stored by this tag, it will be left
+blank.
+*/
+func (t *TimestapTZTag) GetTimeZone() *time.Location {
+	return time.FixedZone("", int(t.Offset)*60)
+}
+
+/*
+Recovers the stored value as a time.Time instance. The returned value will be
+at the local time zone.
+*/
+func (t *TimestapTZTag) GetLocalTimestamp() time.Time {
+	return time.UnixMicro(t.Payload)
+}
+
+/*
+Recovers the stored value as a time.Time instance. The returned value will be
+at the original timezone.
+*/
+func (t *TimestapTZTag) GetTimestamp() time.Time {
+	return t.GetLocalTimestamp().In(t.GetTimeZone())
+}
+
+/*
+Recovers the stored value as a time.Time instance. The returned value will be
+at UTC.
+*/
+func (t *TimestapTZTag) GetTimestampUTC() time.Time {
+	return t.GetTimestamp().UTC()
+}
---
+++ tags/ext/timestamp_test.go
@@ -0,0 +1,396 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright (c) 2022, InterlockLedger Network
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package ext
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/interlockledger/go-iltags/tags"
+	"github.com/interlockledger/go-iltags/tags/impl"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTimestapTag(t *testing.T) {
+	id := tags.TagID(1234)
+	tag := NewTimestapTag(id)
+
+	assert.Equal(t, id, tag.Id())
+	assert.Equal(t, int64(0), tag.Payload)
+
+	assert.Panics(t, func() {
+		NewTimestapTag(tags.TagID(15))
+	})
+}
+
+func TestTimestapTag(t *testing.T) {
+	id := tags.TagID(1234)
+	tag := NewTimestapTag(id)
+
+	// Ensure it has the correct aggreations.
+	_ = tag.ILTagHeaderImpl.Id()
+	_ = tag.SignedILIntPayload.Payload
+}
+
+func TestTimestapTag_SetTimestamp(t *testing.T) {
+	id := tags.TagID(1234)
+
+	tag := NewTimestapTag(id)
+	tag.SetTimestamp(time.UnixMilli(0))
+	assert.Equal(t, int64(0), tag.Payload)
+
+	tag.SetTimestamp(time.UnixMicro(-1234))
+	assert.Equal(t, int64(-1234), tag.Payload)
+
+	tag.SetTimestamp(time.UnixMicro(1234))
+	assert.Equal(t, int64(1234), tag.Payload)
+
+	now := time.Now()
+	tag.SetTimestamp(now)
+	assert.Equal(t, now.UnixMicro(), tag.Payload)
+}
+
+func TestTimestapTag_GetTimestamp(t *testing.T) {
+	now := time.Now()
+
+	id := tags.TagID(1234)
+	tag := NewTimestapTag(id)
+
+	tag.Payload = 0
+	tse := time.UnixMicro(0)
+	ts := tag.GetTimestamp()
+	assert.Equal(t, tse, ts)
+	assert.Equal(t, now.Location(), ts.Location())
+
+	tag.Payload = -123
+	tse = time.UnixMicro(-123)
+	ts = tag.GetTimestamp()
+	assert.Equal(t, tse, ts)
+	assert.Equal(t, now.Location(), ts.Location())
+
+	tag.Payload = 123
+	tse = time.UnixMicro(123)
+	ts = tag.GetTimestamp()
+	assert.Equal(t, tse, ts)
+	assert.Equal(t, now.Location(), ts.Location())
+
+	tag.SetTimestamp(now)
+	ts = tag.GetTimestamp()
+	assert.Equal(t, now.UnixMicro(), ts.UnixMicro())
+	assert.Equal(t, now.Location(), ts.Location())
+}
+
+func TestTimestapTag_GetTimestampUTC(t *testing.T) {
+	now := time.Now().UTC()
+
+	id := tags.TagID(1234)
+	tag := NewTimestapTag(id)
+
+	tag.Payload = 0
+	tse := time.UnixMicro(0).UTC()
+	ts := tag.GetTimestampUTC()
+	assert.Equal(t, tse, ts)
+	assert.Equal(t, now.Location(), ts.Location())
+
+	tag.Payload = -123
+	tse = time.UnixMicro(-123).UTC()
+	ts = tag.GetTimestampUTC()
+	assert.Equal(t, tse, ts)
+	assert.Equal(t, now.Location(), ts.Location())
+
+	tag.Payload = 123
+	tse = time.UnixMicro(123).UTC()
+	ts = tag.GetTimestampUTC()
+	assert.Equal(t, tse, ts)
+	assert.Equal(t, now.Location(), ts.Location())
+
+	tag.SetTimestamp(now)
+	ts = tag.GetTimestampUTC()
+	assert.Equal(t, now.UnixMicro(), ts.UnixMicro())
+	assert.Equal(t, now.Location(), ts.Location())
+}
+
+//------------------------------------------------------------------------------
+
+func TestTimestampTZPayload(t *testing.T) {
+	p := TimestampTZPayload{}
+
+	var _ tags.ILTagPayload = &p
+	_ = p.SignedILIntPayload.Payload
+	_ = p.Offset
+}
+
+func TestTimestampTZPayload_ValueSize(t *testing.T) {
+	p := TimestampTZPayload{}
+
+	assert.Equal(t, uint64(1+2), p.ValueSize())
+
+	p.Payload = 137438953472
+	p.Offset = int16(1)
+	assert.Equal(t, uint64(6+2), p.ValueSize())
+
+	p.Payload = 9223372036854775807
+	p.Offset = int16(32767)
+	assert.Equal(t, uint64(9+2), p.ValueSize())
+
+	p.Payload = -9223372036854775808
+	p.Offset = int16(-32768)
+	assert.Equal(t, uint64(9+2), p.ValueSize())
+}
+
+func TestTimestampTZPayload_SerializeValue(t *testing.T) {
+	p := TimestampTZPayload{}
+
+	w := bytes.NewBuffer(nil)
+	assert.Nil(t, p.SerializeValue(w))
+	assert.Equal(t, []byte{0, 0, 0}, w.Bytes())
+
+	p.Payload = 137438953472
+	p.Offset = int16(1)
+	w = bytes.NewBuffer(nil)
+	assert.Nil(t, p.SerializeValue(w))
+	assert.Equal(t, []byte{0xfc, 0x3f, 0xff, 0xff, 0xff, 0x8, 0x0, 0x1}, w.Bytes())
+
+	p.Payload = 9223372036854775807
+	p.Offset = int16(32767)
+	w = bytes.NewBuffer(nil)
+	assert.Nil(t, p.SerializeValue(w))
+	assert.Equal(t, []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x6, 0x7f, 0xff}, w.Bytes())
+
+	p.Payload = -9223372036854775808
+	p.Offset = int16(-32768)
+	w = bytes.NewBuffer(nil)
+	assert.Nil(t, p.SerializeValue(w))
+	assert.Equal(t, []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7, 0x80, 0x0}, w.Bytes())
+
+	// Simulate errors.
+	p.Payload = 0
+	p.Offset = 0
+	w2 := DummyWriter{0}
+	assert.Error(t, p.SerializeValue(&w2))
+
+	p.Payload = 0
+	p.Offset = 0
+	w2 = DummyWriter{2}
+	assert.Error(t, p.SerializeValue(&w2))
+
+}
+
+func TestTimestampTZPayload_DeserializeValue(t *testing.T) {
+	f := impl.NewStandardTagFactory(true)
+	p := TimestampTZPayload{}
+
+	r := bytes.NewReader([]byte{0, 0, 0})
+	assert.Nil(t, p.DeserializeValue(f, 3, r))
+	assert.Equal(t, int64(0), p.Payload)
+	assert.Equal(t, int16(0), p.Offset)
+
+	r = bytes.NewReader([]byte{0xfc, 0x3f, 0xff, 0xff, 0xff, 0x8, 0x0, 0x1})
+	assert.Nil(t, p.DeserializeValue(f, 8, r))
+	assert.Equal(t, int64(137438953472), p.Payload)
+	assert.Equal(t, int16(1), p.Offset)
+
+	r = bytes.NewReader([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x6, 0x7f, 0xff})
+	assert.Nil(t, p.DeserializeValue(f, 11, r))
+	assert.Equal(t, int64(9223372036854775807), p.Payload)
+	assert.Equal(t, int16(32767), p.Offset)
+
+	r = bytes.NewReader([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7, 0x80, 0x0})
+	assert.Nil(t, p.DeserializeValue(f, 11, r))
+	assert.Equal(t, int64(-9223372036854775808), p.Payload)
+	assert.Equal(t, int16(-32768), p.Offset)
+
+	// Errors
+	r = bytes.NewReader([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7, 0x80, 0x0})
+	assert.ErrorIs(t, p.DeserializeValue(f, 2, r), tags.ErrBadTagFormat)
+
+	r = bytes.NewReader([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7, 0x80, 0x0})
+	assert.ErrorIs(t, p.DeserializeValue(f, 8, r), tags.ErrBadTagFormat)
+
+	r = bytes.NewReader([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7, 0x80, 0x0})
+	assert.ErrorIs(t, p.DeserializeValue(f, 9, r), tags.ErrBadTagFormat)
+
+	r = bytes.NewReader([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7, 0x80, 0x0})
+	assert.ErrorIs(t, p.DeserializeValue(f, 10, r), tags.ErrBadTagFormat)
+
+	r = bytes.NewReader([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7, 0x80, 0x0, 0x00})
+	assert.ErrorIs(t, p.DeserializeValue(f, 12, r), tags.ErrBadTagFormat)
+
+	r = bytes.NewReader([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7, 0x80})
+	assert.ErrorIs(t, p.DeserializeValue(f, 11, r), tags.ErrBadTagFormat)
+}
+
+//------------------------------------------------------------------------------
+
+func TestNewTimestapTZTag(t *testing.T) {
+	id := tags.TagID(1234)
+	tag := NewTimestapTZTag(id)
+
+	assert.Equal(t, id, tag.Id())
+	assert.Equal(t, int64(0), tag.Payload)
+	assert.Equal(t, int16(0), tag.Offset)
+
+	assert.Panics(t, func() {
+		NewTimestapTZTag(tags.TagID(15))
+	})
+}
+
+func TestTimestapTZTag(t *testing.T) {
+	var tag TimestapTZTag
+
+	_ = tag.ILTagHeaderImpl.Id()
+	_ = tag.TimestampTZPayload.Payload
+}
+
+func TestTimestapTZTag_SetTimestamp(t *testing.T) {
+	id := tags.TagID(1234)
+
+	tag := NewTimestapTZTag(id)
+	ts := time.UnixMicro(0).UTC()
+	tag.SetTimestamp(ts)
+	assert.Equal(t, int64(0), tag.Payload)
+	assert.Equal(t, int16(0), tag.Offset)
+
+	ts = time.UnixMicro(-1234).In(time.FixedZone("", 12344))
+	tag.SetTimestamp(ts)
+	assert.Equal(t, int64(-1234), tag.Payload)
+	assert.Equal(t, int16(205), tag.Offset)
+
+	ts = time.UnixMicro(1234).In(time.FixedZone("", -12345))
+	tag.SetTimestamp(ts)
+	assert.Equal(t, int64(1234), tag.Payload)
+	assert.Equal(t, int16(-205), tag.Offset)
+
+	now := time.Now()
+	tag.SetTimestamp(now)
+	assert.Equal(t, now.UnixMicro(), tag.Payload)
+	_, offset := now.Zone()
+	assert.Equal(t, int16(offset/60), tag.Offset)
+}
+
+func TestTimestapTZTag_GetTimestamp(t *testing.T) {
+	now := time.Now()
+
+	id := tags.TagID(1234)
+	tag := NewTimestapTZTag(id)
+
+	tag.Payload = 0
+	tag.Offset = 0
+	tse := time.UnixMicro(0).In(time.FixedZone("", 0))
+	ts := tag.GetTimestamp()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = -123
+	tag.Offset = -1234
+	tse = time.UnixMicro(-123).In(time.FixedZone("", -1234*60))
+	ts = tag.GetTimestamp()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = 123
+	tag.Offset = 1234
+	tse = time.UnixMicro(123).In(time.FixedZone("", 1234*60))
+	ts = tag.GetTimestamp()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = now.UnixMicro()
+	_, offs := now.Zone()
+	tag.Offset = int16(offs / 60)
+	tse = time.UnixMicro(now.UnixMicro()).In(time.FixedZone("", offs))
+	ts = tag.GetTimestamp()
+	assert.Equal(t, tse, ts)
+}
+
+func TestTimestapTZTag_GetLocalTimestamp(t *testing.T) {
+	now := time.Now()
+
+	id := tags.TagID(1234)
+	tag := NewTimestapTZTag(id)
+
+	tag.Payload = 0
+	tag.Offset = 0
+	tse := time.UnixMicro(0)
+	ts := tag.GetLocalTimestamp()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = -123
+	tag.Offset = -1234
+	tse = time.UnixMicro(-123)
+	ts = tag.GetLocalTimestamp()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = 123
+	tag.Offset = 1234
+	tse = time.UnixMicro(123)
+	ts = tag.GetLocalTimestamp()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = now.UnixMicro()
+	_, offs := now.Zone()
+	tag.Offset = int16(offs / 60)
+	tse = time.UnixMicro(now.UnixMicro())
+	ts = tag.GetLocalTimestamp()
+	assert.Equal(t, tse, ts)
+}
+
+func TestTimestapTZTag_GetTimestampUTC(t *testing.T) {
+	now := time.Now().UTC()
+
+	id := tags.TagID(1234)
+	tag := NewTimestapTZTag(id)
+
+	tag.Payload = 0
+	tag.Offset = 0
+	tse := time.UnixMicro(0).UTC()
+	ts := tag.GetTimestampUTC()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = -123
+	tag.Offset = -1234
+	tse = time.UnixMicro(-123).UTC()
+	ts = tag.GetTimestampUTC()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = 123
+	tag.Offset = 1234
+	tse = time.UnixMicro(123).UTC()
+	ts = tag.GetTimestampUTC()
+	assert.Equal(t, tse, ts)
+
+	tag.Payload = now.UnixMicro()
+	_, offs := now.Zone()
+	tag.Offset = int16(offs / 60)
+	tse = time.UnixMicro(now.UnixMicro()).UTC()
+	ts = tag.GetTimestampUTC()
+	assert.Equal(t, tse, ts)
+}
